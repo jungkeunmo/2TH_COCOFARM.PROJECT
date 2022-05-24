aeb3af9d83813cf79c34aeb1eff6bcee83a08190
jungkeunmo <abc59684495@gmail.com>
2022-05-24 12:59:46 +0900

    farm

--- front/components/Footer.jsx
+++ front/components/Footer.jsx
@@ -3,13 +3,17 @@ import styled from "styled-components";
 
 const FooterBox = styled.div`
     width: 100%;
-    height: 400px;
+    height: 500px;
+    
+    padding: 50px 50px;
 
-    background-image: linear-gradient( rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.5)), url("https://i.pinimg.com/474x/e0/87/c6/e087c61ad0682c9fa4c8a9d1ba7d6834.jpg");
-    color: #fff;
-
+    background-image: linear-gradient( rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.6)), url("https://i.pinimg.com/474x/ce/7e/59/ce7e597236d70c8f1e23a9f724e9f64b.jpg");
     background-size: cover;
     background-attachment: fixed;
+    background-repeat: no-repeat;
+    background-position: center center;
+
+    color: #fff;
 `;
 
 const Footer = () => {
