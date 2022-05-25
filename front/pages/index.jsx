5f15d6284db9ca9ab4d0c8e0270eb8f5908a7a76
jungkeunmo <abc59684495@gmail.com>
2022-05-25 17:41:00 +0900

    farm

--- front/pages/index.jsx
+++ front/pages/index.jsx
@@ -4,7 +4,7 @@ import {} from "antd";
 
 const Body = styled.div`
     width: 100%;
-    height: 460vh;
+    height: 515vh;
 
     display: flex;
     flex-direction: column;
@@ -101,7 +101,7 @@ const Wrapper2__Box = styled.div`
 
 const Wrapper3 = styled.div`
   width: 100%;
-  height: 110vh;
+  height: 150vh;
   background-color: #226cac;
 
   padding: 0px 130px;
@@ -123,27 +123,78 @@ const Wrapper3__Box1 = styled.div`
   flex-direction: row;
 `;
 
+const Wrapper3__BoxText = styled.div`
+  width: 100%;
+  height: 300px;
+  background-color: #333d46;
+
+  padding: 0px 130px;
+
+  display: flex;
+  flex-direction: row;
+
+  border-bottom: 10px solid #000;
+`;
+
+
 const Wrapper3__Img = styled.img`
   width: 450px;
   height: 350px;
+
   background-color: #486075;
+
   margin: 20px;
 `;
 
 const Wrapper4 = styled.div`
   width: 100%;
-  height: 95vh;
+  height: 100vh;
+
   background-color: #101820;
+
+  padding: 0px 130px;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper4__Box = styled.div`
+  width: 80%;
+  height: 75%;
+  
+  background-color: #3b5a7a;
 
   padding: 0px 130px;
 `;
 
 const Wrapper5 = styled.div`
   width: 100%;
-  height: 55vh;
+  height: 15vh;
   background-color: #134673;
+
   padding: 0px 130px;
 `;
+
+const Wrapper6 = styled.div`
+  width: 100%;
+  height: 55vh;
+  background-color: #3a75a8;
+
+  padding: 0px 130px;
+
+  display: flex;
+  flex-direction: row;
+`;
+
+const Wrapper6__img = styled.div`
+  width: 100%;
+  height: 55vh;
+  background-color: #3a75a8;
+  padding: 0px 130px;
+`;
+
 
 const MyWeb = () => {
     return (
@@ -168,7 +219,7 @@ const MyWeb = () => {
                 <Wrapper2__Box></Wrapper2__Box>
             </Wrapper2>
             <Wrapper3>
-                <Wrapper1__Titile style={{ padding: "" }}>COCOFARM</Wrapper1__Titile>  
+                <Wrapper1__Titile>COCOFARM</Wrapper1__Titile>  
                 <Wrapper1__Desc>Lorem ipsum dolor sit amet consectetur adipisicing elit. temporibus laudantium</Wrapper1__Desc>
 
                 <Wrapper3__Box1>
@@ -177,14 +228,21 @@ const MyWeb = () => {
                     <Wrapper3__Img></Wrapper3__Img>
                 </Wrapper3__Box1>
 
-                <Wrapper3__Box1>
+                <Wrapper3__BoxText>
                     <Wrapper1__Desc>Lorem ipsum dolor sit amet consectetur adipisicing elit. temporibus laudantium</Wrapper1__Desc>
                     <Wrapper1__Desc>Lorem ipsum dolor sit amet consectetur adipisicing elit. temporibus laudantium</Wrapper1__Desc>
                     <Wrapper1__Desc>Lorem ipsum dolor sit amet consectetur adipisicing elit. temporibus laudantium</Wrapper1__Desc>
-                </Wrapper3__Box1>    
+                </Wrapper3__BoxText>  
+
+                <Wrapper1__Titile style={{ borderButton: "1pxsoild#000" }}>COCOFARM</Wrapper1__Titile>  
+                <Wrapper1__button>공지사항</Wrapper1__button>
             </Wrapper3>
-            <Wrapper4></Wrapper4>
+            <Wrapper4>
+                <Wrapper4__Box></Wrapper4__Box>
+            </Wrapper4>
             <Wrapper5></Wrapper5>
+            <Wrapper6>
+            </Wrapper6>
         </Body>
     );
 }
