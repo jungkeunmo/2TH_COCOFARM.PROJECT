8b2524e6c402770a4cd386eb4e5bc311f618b0f5
jungkeunmo <abc59684495@gmail.com>
2022-08-05 19:09:25 +0900

    COCOFARM

--- front/pages/admin/index.jsx
+++ front/pages/admin/index.jsx
@@ -5,24 +5,25 @@ import {} from "antd";
 
 const Wrapper = styled.div`
     width: 100%;
-    height: 100vh;
-    
+
     background-color: red;
 
     display: flex;
     flex-direction: column;
-    align-items: center;
+    padding: 15px;
 `;
 
 const TopBox = styled.div`
-    width: 80%;
-    height: 25%;
+    width: 100%;
+    height: 170px;
 
-    background-color: aqua;
+    background: linear-gradient( to left, #d37272 15%, #753b3b 85% );
+
+    border-radius: 15px;
 `;
 
 const BottomBox = styled.div`
-    width: 80%;
+    width: 100%;
     height: 75%;
 
     background-color: aquamarine;
@@ -32,14 +33,28 @@ const BottomBox = styled.div`
 `;
 
 const BottomLift = styled.div`
-    width: 40%;
+    width: 35%;
     height: 100%;
 
     background-color: #7ea095;
+
+    display: flex;
+    flex-direction: column;
+`;
+
+const BLBox = styled.div`
+    width: 95%;
+    height: ${(props) => props.height || "350px"};
+
+    background-color: aqua;
+
+    margin-top: 10px;
+
+    border-radius: 14px;
 `;
 
 const BottomRight = styled.div`
-    width: 60%;
+    width: 65%;
     height: 100%;
 
     background-color: #7e7a6a;
@@ -50,7 +65,11 @@ const AdminIndex = () => {
         <Wrapper>
             <TopBox></TopBox>
             <BottomBox>
-                <BottomLift></BottomLift>
+                <BottomLift>
+                    <BLBox height="400px"></BLBox>
+                    <BLBox></BLBox>
+
+                </BottomLift>
                 <BottomRight></BottomRight>
             </BottomBox>
         </Wrapper>
